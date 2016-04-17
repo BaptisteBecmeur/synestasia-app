2b96f3d0d38d90d7a3504270706f993ca1f2afcb
Baptiste Becmeur <baptistebecmeur@gmail.com>
2016-04-17 16:44:53 +0200

    post_category collection done + index and show view

--- app/models/post_category.rb
+++ app/models/post_category.rb
@@ -1,4 +1,7 @@
 class PostCategory < ActiveRecord::Base
   belongs_to :post
-  validates :name, presence: true
+  # validates :name, presence: true
+
+   NAMES = ["Japon", "Chine", "Corée du Sud", "Moyen-Orient"]
+   validates :name, inclusion: { in: PostCategory::NAMES, allow_nil: false }
 end
