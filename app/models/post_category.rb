28b1b99845331d8062ba841f7de6d5c1240a7db4
Baptiste Becmeur <baptistebecmeur@gmail.com>
2016-04-20 09:48:47 +0200

    minor modifications

--- app/models/post_category.rb
+++ app/models/post_category.rb
@@ -1,7 +1,6 @@
 class PostCategory < ActiveRecord::Base
   has_many :posts, dependent: :destroy
 
-
   NAMES = ["Japon", "Chine", "Corée du Sud", "Moyen Orient", "Indien"]
   validates :name, inclusion: { in: PostCategory::NAMES, allow_nil: false }
 end
