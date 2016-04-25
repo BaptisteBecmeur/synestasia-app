37eef43d7b23056ff9a407352c0d1b81a43c134e
Baptiste Becmeur <baptistebecmeur@gmail.com>
2016-04-25 22:48:08 +0200

    views assets symboles

--- app/authorizers/symbole_authorizer.rb
+++ app/authorizers/symbole_authorizer.rb
@@ -4,7 +4,7 @@ class SymboleAuthorizer < ApplicationAuthorizer
     user.present? #and user.has_any_role?(:admin, :teacher))
   end
   def self.updatable_by?(user)
-    (user.present? and user.has_any_role?(:admin, :teacher))
+    user.present? #and user.has_any_role?(:admin, :teacher))
   end
   def self.deletable_by?(user)
     (user.present? and user.has_any_role?(:admin, :teacher))
